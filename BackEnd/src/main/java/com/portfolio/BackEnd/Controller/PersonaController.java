cf5072b3cfb853fef390336bfac80680362c9bb2
Sergio Durelli <dredd_arg@hotmail.com>
2023-05-20 17:27:20 -0300

    BackEnd Persona -> listar, crear, editar y borrar

---
+++ BackEnd/src/main/java/com/portfolio/BackEnd/Controller/PersonaController.java
@@ -0,0 +1,55 @@
+package com.portfolio.BackEnd.Controller;
+
+import com.portfolio.BackEnd.Entity.Persona;
+import com.portfolio.BackEnd.Interface.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonaController {
+    @Autowired IPersonaService ipersonaServ;
+    
+    @GetMapping("personas/lista")
+    public List<Persona> getPersona(){
+        return ipersonaServ.getPersona();
+    }
+    
+    @PostMapping("personas/crear")
+    public String savePersona(@RequestBody Persona persona){
+        ipersonaServ.savePersona(persona);
+        return "Persona creada.";
+    }
+    
+    @DeleteMapping("personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaServ.deletePersona(id);
+        return "Persona borrada.";
+    }
+    
+    @PutMapping("personas/editar/{id}")
+    public String editPersona(@PathVariable Long id,
+                              @RequestParam("nombre") String newnombre,
+                              @RequestParam("bio") String newbio,
+                              @RequestParam("longbio") String newlongbio,
+                              @RequestParam("fotoPerfilUrl") String newfotoPerfilUrl
+                              ){
+        Persona newpersona = ipersonaServ.findPersona(id);
+        newpersona.setNombre(newnombre);
+        newpersona.setBio(newbio);
+        newpersona.setLongBio(newlongbio);
+        newpersona.setFotoPerfilUrl(newfotoPerfilUrl);
+        
+        ipersonaServ.savePersona(newpersona);
+        
+        return "Persona modificada.";
+    }
+    
+}
