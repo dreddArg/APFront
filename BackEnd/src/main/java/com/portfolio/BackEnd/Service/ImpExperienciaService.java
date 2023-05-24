dbfc9c33c0b1b0ba41c54df0abd27229a36ff052
Sergio Durelli <dredd_arg@hotmail.com>
2023-05-24 00:21:34 -0300

    add: Experiencia Entity

---
+++ BackEnd/src/main/java/com/portfolio/BackEnd/Service/ImpExperienciaService.java
@@ -0,0 +1,36 @@
+package com.portfolio.BackEnd.Service;
+
+import com.portfolio.BackEnd.Entity.Experiencia;
+import com.portfolio.BackEnd.Repository.IExperienciaRepository;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ImpExperienciaService {
+    @Autowired
+    IExperienciaRepository iExperienciaRepository;
+    
+    public List<Experiencia> list(){
+        return iExperienciaRepository.findAll();
+    }
+    
+    public Optional<Experiencia> getOne(int id){
+        return iExperienciaRepository.findById(id);
+    }
+    
+    public Optional<Experiencia> getByNombreEmpresa(String nombreEmpresa){
+        return iExperienciaRepository.findByNombreEmpresa(nombreEmpresa);
+    }
+    
+    public void save(Experiencia expe){
+        iExperienciaRepository.save(expe);
+    }
+    
+    public void delete(int id){
+        iExperienciaRepository.deleteById(id);
+    }
+}
