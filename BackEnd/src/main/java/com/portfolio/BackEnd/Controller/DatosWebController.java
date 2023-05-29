bee990526a58369d348bb35ab949344a2784c1a7
Sergio Durelli <dredd_arg@hotmail.com>
2023-05-28 21:43:15 -0300

    update: arreglos menores

--- BackEnd/src/main/java/com/portfolio/BackEnd/Controller/DatosWebController.java
+++ BackEnd/src/main/java/com/portfolio/BackEnd/Controller/DatosWebController.java
@@ -8,20 +8,23 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
+@RequestMapping("/api")
+
 public class DatosWebController {
     @Autowired IDatosWebService iDatosWebService;
     
-    @GetMapping("api/getdatosweb")
+    @GetMapping("/getdatosweb")
     private List<DatosWeb> getDatosWeb(){
         return iDatosWebService.getDatosWeb();
     }
     
-    @PostMapping("api/savedatosweb")
+    @PostMapping("/savedatosweb")
     private void saveDatosWeb(@RequestBody DatosWeb datosWeb){
         iDatosWebService.saveDatosWeb(datosWeb);
     }
--- BackEnd/src/main/java/com/portfolio/BackEnd/Controller/RedesSocialesController.java
+++ BackEnd/src/main/java/com/portfolio/BackEnd/Controller/RedesSocialesController.java
@@ -8,21 +8,24 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
+@RequestMapping("/api")
+
 public class RedesSocialesController {
     
     @Autowired IRedesSocialesService iRedesSocialesService;
     
-    @GetMapping("api/getredessociales")
+    @GetMapping("/getredessociales")
     public List<RedesSociales> getRedesSociales(){
         return iRedesSocialesService.getRedesSociales();
     }
     
-    @PostMapping("api/saveredessociales")
+    @PostMapping("/saveredessociales")
     public void saveRedesSociales(@RequestBody RedesSociales redesSociales){
         iRedesSocialesService.saveRedesSociales(redesSociales);
     }
