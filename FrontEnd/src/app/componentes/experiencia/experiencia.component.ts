5c3b5870ee51988012f70e0262edc9dbf113b92d
Sergio Durelli <dredd_arg@hotmail.com>
2023-06-01 20:27:33 -0300

    add: Experiencia add-edit-delete works now

--- FrontEnd/src/app/componentes/experiencia/experiencia.component.ts
+++ FrontEnd/src/app/componentes/experiencia/experiencia.component.ts
@@ -1,28 +1,58 @@
-import { Component } from '@angular/core';
-import { PortfolioService } from 'src/app/servicios/portfolio.service';
+import { Component, OnInit } from '@angular/core';
+import { DatosWeb } from 'src/app/model/datos-web';
+import { Experiencia } from 'src/app/model/experiencia';
+import { DatosWebService } from 'src/app/servicios/datos-web.service';
+import { ExperienciaService } from 'src/app/servicios/experiencia.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
-export class ExperienciaComponent {
-  datosWeb:any;
-  miPortfolio:any;
-  experiencia:any;
+export class ExperienciaComponent implements OnInit {
 
-  constructor(private datosPortfolio:PortfolioService) { }
+  datosWeb: DatosWeb = new DatosWeb("", "", "", "", "", "", "", "","","", 0);
+  exp: Experiencia[] = [];
+
+  constructor(private datosWebService: DatosWebService, private sExperiencia: ExperienciaService, private tokenService: TokenService) { }
+
+  isLogged = false;
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatosWeb().subscribe(data =>{
-      //console.log(data);
-      this.datosWeb = data[0];
-      //this.redesSociales = data.redesSociales;
-      // console.log(this.redesSociales)
-    });
-    
-    this.datosPortfolio.obtenerExperiencia().subscribe(data => {
-      this.experiencia = data;
-    });
+
+    //cargamos experiencia
+    this.cargarExperiencia();
+
+    //cargamos datosWeb
+    this.datosWebService.obtenerDatosWeb().subscribe(data =>{this.datosWeb = data});
+
+    // validamos si estamos logueados o no
+    if (this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+
+  }
+
+  cargarExperiencia(): void {
+    this.sExperiencia.listaExp().subscribe(data => {this.exp = data})
+  }
+
+  delete(id?: number) {
+    //revisamos q el id esté definido para borrar
+    if (id != undefined ) {
+      // en ese caso procedemos a borrar
+      this.sExperiencia.deleteExp(id).subscribe (
+        data => {
+          // recargamos los datos a la web
+          this.cargarExperiencia();
+        }, err => {
+          // si hubo error
+          alert("No se pudo eliminar el registro")
+        }
+      )
+    }
   }
 }
